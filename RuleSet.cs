5ec1eee9d25dc6877f85109844a029d9be26cdd1
Alberto Amoruso <alberto.am@me.com>
2021-11-22 22:50:56 +0100

    Added `RuleSet`

---
+++ RuleSet.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class RuleSet<T>
+{
+    public abstract HashSet<Rule<T>> rules { get; }
+
+    public ValidationResult Validate(T data)
+    {
+        var rule = rules.First();
+        rules.Remove(rule);
+
+        foreach (var r in rules)
+            rule.AddRule(r);
+
+        return rule.Validate(data);
+    }
+}
