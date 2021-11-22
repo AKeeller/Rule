35c44d713a60621c051ec7250b7510e72bd16376
Alberto Amoruso <alberto.am@me.com>
2021-11-22 22:49:50 +0100

    Added `Rule`

---
+++ Rule.cs
@@ -0,0 +1,30 @@
+public abstract class Rule<T>
+{
+    public Rule<T> Next { get; set; }
+
+    protected abstract ValidationResult PartialValidation(T data);
+
+    public ValidationResult Validate(T data)
+    {
+        var result = Next is null ? new() : Next.Validate(data);
+
+        var partialResult = PartialValidation(data);
+
+        result.IsValid &= partialResult.IsValid;
+        result.Messages.AddRange(partialResult.Messages);
+
+        return result;
+    }
+
+    public Rule<T> AddRule(Rule<T> next)
+    {
+        Rule<T> lastRule = this;
+
+        while (lastRule.Next is not null)
+            lastRule = lastRule.Next;
+
+        lastRule.Next = next;
+
+        return this;
+    }
+}
