36bd378306cbd8ddd94abbea3c0c2afbbee4b7f8
Alberto Amoruso <alberto.am@me.com>
2021-12-02 19:33:02 +0100

    Added new `+` operator to `ValidationResult`

--- src/ValidationResult.cs
+++ src/ValidationResult.cs
@@ -1,9 +1,17 @@
 namespace AKeeller.Rule;
 
 using System.Collections.Generic;
+using System.Linq;
 
 public struct ValidationResult
 {
 	public bool IsValid = true;
 	public List<string> Messages = new();
+
+	public static ValidationResult operator +(ValidationResult a, ValidationResult b) =>
+		new()
+		{
+			IsValid = a.IsValid & b.IsValid,
+			Messages = a.Messages.Concat(b.Messages).ToList()
+		};
 }
