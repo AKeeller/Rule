08091a61b8cc3f14d3250a6586c1689dca80d204
Alberto Amoruso <alberto.am@me.com>
2021-12-05 23:22:30 +0100

    Simplified `RuleSetTest`

--- test/RuleSetTest.cs
+++ test/RuleSetTest.cs
@@ -13,28 +13,13 @@ public class RuleSetTest
 	}
 
 	[TestMethod]
-	public void NotValidTest1()
+	[DataRow(10)]
+	[DataRow(10000001)]
+	[DataRow(1)]
+	public void NotValidTest(int value)
 	{
 		var ruleSet = new IsBigAndEven();
-		var result = ruleSet.Validate(10);
-
-		Assert.IsFalse(result.IsValid);
-	}
-
-	[TestMethod]
-	public void NotValidTest2()
-	{
-		var ruleSet = new IsBigAndEven();
-		var result = ruleSet.Validate(10000001);
-
-		Assert.IsFalse(result.IsValid);
-	}
-
-	[TestMethod]
-	public void NotValidTest3()
-	{
-		var ruleSet = new IsBigAndEven();
-		var result = ruleSet.Validate(1);
+		var result = ruleSet.Validate(value);
 
 		Assert.IsFalse(result.IsValid);
 	}
